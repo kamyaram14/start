65b2bb3f55c760f3d78139ca5f3148bdc9db1f71
kamyaram14 <130030904+kamyaram14@users.noreply.github.com>
2023-05-11 05:22:14 +0330

    debug

--- statrt.sh
+++ statrt.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-sudo apt update &> /dev/null && sudo apt upgrade &> /dev/null
-sudo ufw allow 'ssh' &> /dev/null
-sudo ufw enable &> /dev/null
+sudo apt update && sudo apt upgrade
+sudo ufw allow 'ssh'
+sudo ufw enable
 sudo ufw status
 sudo apt install docker 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" &> /dev/null
-sudo apt update &> /dev/null
-apt-cache policy docker-ce &> /dev/null
-sudo apt install docker-ce &> /dev/null
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo chown -R $USER:$USER /var/www
 sudo docker image build -t kamyarkalhor.ir .
 sudo docker compose up -d
-sudo chown -R $USER:$USER /var/www
-git clone https://github.com/Voorivex/OOB-Server.git &> /dev/null
-sudo chmod +x ./OOB-Server/setup
-read -p "Enter Your Domain: " domain
-read -p "Enter Your ServerIP: " ServerIP
-sudo ./OOB-Server/setup domain ServerIP
-sudo chown -R $USER:$USER /etc/bind
-sudo echo "local            IN      A       127.0.0.1" >> /etc/bind/db.local
-sudo echo "aws-ssrf         IN      A       169.254.169.254" >> /etc/bind/db.local
-dig +short domain
-read -p "Would you like to also install tools and GO? (y)es or (n)o " cont
-if [[ "$cont" == "n" ]]; then
+
+read -p "Would you like to also install OOB-Server? (y)es or (n)o " cont_1
+if [[ "$cont_1" == "n" ]]; then
+    echo "OK"
+else
+    chmod +x ./install_OOB
+    ./install_OOB
+fi
+
+read -p "Would you like to also install tools and GO? (y)es or (n)o " cont_2
+if [[ "$cont_2" == "n" ]]; then
     echo "OK"
 else
     chmod +x ./tools
